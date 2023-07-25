a5a68a009532e0a9ea07121e8696a25725959f5a
ilandbz <ilandbz@gmail.com>
2023-07-25 17:40:21 -0500

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <div>
     <footer class="main-footer">
-        <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Cristian Figueroa Ferrer</a>.</strong>
-        All rights reserved.
+        <strong>Copyright &copy; 2023 <a href="https://adminlte.io">Papitas Like </a>.</strong>
+        Todos los derechos reservados.
         <div class="float-right d-none d-sm-inline-block">
           <b>Version</b> 3.2.0
         </div>
